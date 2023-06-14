99cbb6dea45e39da3c886420586b348225869de3
eric <ericarc99@gmail.com>
2023-06-14 16:37:31 +0800

    註冊與登錄

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
   <div class="container">
     <p class="float-start">
-      由 <a href="https://eric888.com" target="_blank">Eric</a> 設計 <span style="color: #e27575;font-size: 14px;">❤</span>
+      由 <a href="https://eric888.com" target="_blank">Eric</a> 設計
     </p>
     <p class="float-end"><a href="mailto:test@email.com">聯繫我們</a></p>
   </div>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+//首頁
 Route::get('/', 'PagesController@root')->name('root');
+
+/*登入、註冊
+| GET|HEAD | login                  | login            | App\Http\Controllers\Auth\LoginController@showLoginForm                | web                                         |
+|          |                        |                  |                                                                        | App\Http\Middleware\RedirectIfAuthenticated |
+| POST     | login                  |                  | App\Http\Controllers\Auth\LoginController@login                        | web                                         |
+|          |                        |                  |                                                                        | App\Http\Middleware\RedirectIfAuthenticated |
+| POST     | logout                 | logout           | App\Http\Controllers\Auth\LoginController@logout                       | web                                         |
+| GET|HEAD | password/confirm       | password.confirm | App\Http\Controllers\Auth\ConfirmPasswordController@showConfirmForm    | web                                         |
+|          |                        |                  |                                                                        | App\Http\Middleware\Authenticate            |
+| POST     | password/confirm       |                  | App\Http\Controllers\Auth\ConfirmPasswordController@confirm            | web                                         |
+|          |                        |                  |                                                                        | App\Http\Middleware\Authenticate            |
+| POST     | password/email         | password.email   | App\Http\Controllers\Auth\ForgotPasswordController@sendResetLinkEmail  | web                                         |
+| GET|HEAD | password/reset         | password.request | App\Http\Controllers\Auth\ForgotPasswordController@showLinkRequestForm | web                                         |
+| POST     | password/reset         | password.update  | App\Http\Controllers\Auth\ResetPasswordController@reset                | web                                         |
+| GET|HEAD | password/reset/{token} | password.reset   | App\Http\Controllers\Auth\ResetPasswordController@showResetForm        | web                                         |
+| GET|HEAD | register               | register         | App\Http\Controllers\Auth\RegisterController@showRegistrationForm      | web                                         |
+|          |                        |                  |                                                                        | App\Http\Middleware\RedirectIfAuthenticated |
+| POST     | register               |                  | App\Http\Controllers\Auth\RegisterController@register                  | web                                         |
+|          |                        |                  |                                                                        | App\Http\Middleware\RedirectIfAuthenticated |
+*/
+Auth::routes();
+
+//
 
 
