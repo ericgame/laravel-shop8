d5e55a1c97c857b357a6cd65f49d704cfa0be54d
eric <ericarc99@gmail.com>
2023-07-10 17:24:26 +0800

    用戶界面 - 商品篩選、排序

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -9,8 +9,48 @@ class ProductsController extends Controller
 {
     public function index(Request $request)
     {
-        $products = Product::query()->where('on_sale', true)->paginate(16);
+        // $products = Product::query()->where('on_sale', true)->paginate(16);
 
-        return view('products.index', ['products' => $products]);
+        // 創建一個查詢構造器
+        $builder = Product::query()->where('on_sale', true);
+
+        // 判斷是否有提交 search 參數，如果有就賦值給 $search 變量
+        // search 參數用來模糊搜索商品
+        if ($search = $request->input('search', '')) {
+            $like = '%'.$search.'%';
+
+            // 模糊搜索商品標題、商品詳情、SKU 標題、SKU描述
+            $builder->where(function ($query) use ($like) {
+                $query->where('title', 'like', $like)
+                    ->orWhere('description', 'like', $like)
+                    ->orWhereHas('skus', function ($query) use ($like) {
+                        $query->where('title', 'like', $like)
+                            ->orWhere('description', 'like', $like);
+                    });
+            });
+        }
+
+        // 是否有提交 order 參數，如果有就賦值給 $order 變量
+        // order 參數用來控製商品的排序規則
+        if ($order = $request->input('order', '')) {
+            // 是否是以 _asc 或者 _desc 結尾
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+                // 如果字符串的開頭是這 3 個字符串之一，說明是一個合法的排序值
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    // 根據傳入的排序值來構造排序參數
+                    $builder->orderBy($m[1], $m[2]);
+                }
+            }
+        }
+
+        $products = $builder->paginate(16);
+
+        return view('products.index', [
+            'products' => $products,
+            'filters'  => [
+                'search' => $search,
+                'order' => $order,
+            ],
+        ]);
     }
 }
